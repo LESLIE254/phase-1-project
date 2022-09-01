31d46e21c70bdf6ca0b80cad4c84da49c5682123
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-09-01 12:22:11 +0300

    Patch request

--- script.js
+++ script.js
@@ -49,21 +49,28 @@ function renderAnimal(animal) {
     <div class-"content">
         <h4>${animal.name}</h4>
         <p>
-            $<span class="donation-count">${animal.donations = 0}</span> Donated
+            KES<span class="donation-count">${animal.donations = 0}</span> Donated
         </p>
         <p>latin-name:${animal.latin_name}, animal_type:${animal.animal_type}, active_time:${animal.active_time}, length_min:${animal.length_min}, length_max:${animal.length_max}, weight_min:${animal.weight_min}, weight_max:${animal.weight_max}, lifespan:${animal.lifespan}, habitat:${animal.habitat},
         diet:${animal.diet},geo_range:${animal.geo_range}</p>
     </div>
     <div class="buttons">
-        <button>Donate 1000 KES </button>
-        <button> Set Free </button>
+        <button id="donate">Donate 1000 KES </button>
+        <button id = "set_free"> SetFree </button>
     </div>    
     `
    //let img = document.querySelector('img')
    //card.classList.add('cards')
     //Add cards to DOM
     document.querySelector('#animal-list').appendChild(card)
+    card.querySelector('#donate').addEventListener('click', () => {
+        animal.donations+= 1000
+        card.querySelector('span').textContent = animal.donations
+        updateDonations(animal)
+    })
 }
+
+
 //Fetch Request
 function getAllAnimals() {
     fetch("https://zoo-animal-api.herokuapp.com/animals/rand/5")
@@ -76,6 +83,18 @@ function adoptAnimal(animalObject) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(animalObject)
+    })
+    .then(res => res.json())
+    .then(animal => console.log(animal))
+}
+
+function updateDonations(animalObject) {
+    fetch(`https://zoo-animal-api.herokuapp.com/animals/rand/5/${animalObject.id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-Type': 'application/json'
         },
         body:JSON.stringify(animalObject)
     })
