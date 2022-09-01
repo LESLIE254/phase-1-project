570d84628da697864d1ef00e8252e94c3924d33b
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-09-01 11:44:17 +0300

    Submit event listener

--- script.js
+++ script.js
@@ -11,6 +11,35 @@
 
 }
 zooData()*/
+
+//Event listeners
+document.querySelector('#zoo-form').addEventListener('submit',handleSubmit)
+
+//Event Handlers
+function handleSubmit(e) {
+    e.preventDeafult()
+    let animalObject = {
+        name: e.target.name.value,
+        image_link: e.target.image_link.value,
+        //donations:0,
+        //latin_name: e.target.latin_name.value,
+       // animal_type: e.target.animal_type.value,
+        //active_time: e.target.active_time.value,
+        //length_min: e.target.length_min.value,
+        //length_max: e.target.length_max.value,
+        //weight_min: e.target.weight_min.value,
+        //weight_max: e.target.weight_max.value,
+        //lifespan: e.target.lifespan.value,
+        //habitat: e.target.habitat.value,
+        //diet: e.target.diet.value,
+        //geo_range: e.target.geo_range.value
+    }
+    renderAnimal(animalObject)
+}
+
+
+
+
 function renderAnimal(animal) {
     let card = document.createElement('li')
    card.style.marginLeft= '500px'
@@ -20,9 +49,9 @@ function renderAnimal(animal) {
     <div class-"content">
         <h4>${animal.name}</h4>
         <p>
-            $<span class="donation-count">${animal.donations}</span> Donated
+            $<span class="donation-count">${animal.donations = 0}</span> Donated
         </p>
-        <p>latin-name:${animal.animal_type}, animal_type:${animal.animal_type}, active_time:${animal.active_time}, length_min:${animal.length_min}, length_max:${animal.length_max}, weight_min:${animal.weight_min}, weight_max:${animal.weight_max}, lifespan:${animal.lifespan}, habitat:${animal.habitat},
+        <p>latin-name:${animal.latin_name}, animal_type:${animal.animal_type}, active_time:${animal.active_time}, length_min:${animal.length_min}, length_max:${animal.length_max}, weight_min:${animal.weight_min}, weight_max:${animal.weight_max}, lifespan:${animal.lifespan}, habitat:${animal.habitat},
         diet:${animal.diet},geo_range:${animal.geo_range}</p>
     </div>
     <div class="buttons">
@@ -35,10 +64,23 @@ function renderAnimal(animal) {
     //Add cards to DOM
     document.querySelector('#animal-list').appendChild(card)
 }
+//Fetch Request
 function getAllAnimals() {
     fetch("https://zoo-animal-api.herokuapp.com/animals/rand/5")
     .then(resp => resp.json())
     .then(animalData => animalData.forEach(animal => renderAnimal(animal)))
+}
+
+function adoptAnimal(animalObject) {
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand/5", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(animalObject)
+    })
+    .then(res => res.json())
+    .then(animal => console.log(animal))
 }
 //getAllAnimals()
 
